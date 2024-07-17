02f3d7700f9d16b7259f07e0c5a9c5aebfdaa432
Yu-jiin <jiin4083@naver.com>
2024-07-17 15:31:14 +0900

    5번째 실습 완료

---
+++ practice/ws_3_2.py
@@ -0,0 +1,20 @@
+number_of_people = 0
+
+print(f'현재 가입 된 유저 수 : {number_of_people}')
+
+def increase_user():
+    global number_of_people
+    number_of_people += 1
+    
+def create_user(name,age,address):
+    increase_user()
+    user_info = {
+        'name' : name,
+        'age' : age,
+        'address' : address
+    }
+    return user_info
+
+result = create_user(name='홍길동', age=30, address='서울')
+print(result)
+print(f'현재 가입 된 유저 수 : {number_of_people}')
