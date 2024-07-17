badd464df247c9602487f68c15921340a09bdb07
Yu-jiin <jiin4083@naver.com>
2024-07-17 09:26:15 +0900

    첫번째 실습

---
+++ practice/01-basic.py
@@ -0,0 +1,14 @@
+def make_sum(pram1, pram2):
+    """이것은 두 수를 받아
+    두 수의 합을 반환하는 함수입니다.
+    >>> make_sum(1, 2)
+    3
+    """
+    return pram1 + pram2
+
+result = make_sum(100,30)
+return_value = print(result)
+# print 함수는 return이 없다.  값 None으로 나옴 
+print(return_value)
+
+
---
+++ practice/02-arguments.py
@@ -0,0 +1,25 @@
+# 1. Positional Arguments
+
+
+# 2. Default Argument Values
+
+
+# 3. Keyword Arguments
+
+
+# 4. Arbitrary Argument Lists
+
+
+# 5. Arbitrary Keyword Argument Lists
+
+
+# 인자의 모든 종류를 적용한 예시
+def func(pos1, pos2, default_arg='default', *args, **kwargs):
+    print('pos1:', pos1)
+    print('pos2:', pos2)
+    print('default_arg:', default_arg)
+    print('args:', args)
+    print('kwargs:', kwargs)
+
+
+func(1, 2, 3, 4, 5, 6, key1='value1', key2='value2')
