3ecb567ec350dd868e36fc66e1b071ed480b4108
Yu-jiin <jiin4083@naver.com>
2024-07-22 15:03:21 +0900

    5번째 실습 완료

--- practice/240722/ws_5_c.py
+++ practice/240722/ws_5_c.py
@@ -1,8 +1,26 @@
 def restructure_word(word, arr):
-    pass
+    for item in word:
+        if item.isdecimal():
+            for _ in range(int(item)):
+                if arr:
+                    arr.pop()
+        else: 
+            while item in arr:
+                arr.remove(item)    
+    return arr  
+
 
 original_word = '코딩 공부는ㄴ 1일ㄹ 1커ㅓ밋ㅅ @@@#^()#_+!&~:"'
 word = '1ㄴ2ㄹ3ㅓ4ㅅ5'
 arr = []
+    
+for work in original_word:
+    arr.extend(work)
+print(arr)
+
 
 result = restructure_word(word, arr)
+print(result)
+
+text = ''.join(result)
+print(text)
