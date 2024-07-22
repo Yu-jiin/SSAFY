061e5b24b41588a5f39f9872992a2649096818d9
Yu-jiin <jiin4083@naver.com>
2024-07-22 11:39:08 +0900

    실습과제 Lv1 (2) 완료

---
+++ practice/240722/hw_5_2.py
@@ -0,0 +1,7 @@
+# 아래 함수를 수정하시오.
+def count_character():
+    pass
+
+
+result = count_character("Hello, World!", "o")
+print(result)  # 2
---
+++ practice/240722/ws_5_1.py
@@ -0,0 +1,9 @@
+# 아래 함수를 수정하시오.
+def reverse_string(a):
+    return ''.join(reversed(a))
+    
+    
+
+
+result = reverse_string("Hello, World!")
+print(result)  # !dlroW ,olleH
