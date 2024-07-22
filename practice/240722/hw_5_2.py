db613699621199c8964eae05fc98485182c5a945
Yu-jiin <jiin4083@naver.com>
2024-07-22 12:08:11 +0900

    과제 1번 완료

--- practice/240722/hw_5_2.py
+++ practice/240722/hw_5_2.py
@@ -1,7 +1,8 @@
 # 아래 함수를 수정하시오.
-def count_character():
-    pass
+def count_character(a,b):
+    count = a.count(b)
+    return count
 
-
+ 
 result = count_character("Hello, World!", "o")
 print(result)  # 2
