aed1d15c48cac59053a3030959fd0f44b5baf5f2
Yu-jiin <jiin4083@naver.com>
2024-07-22 09:19:26 +0900

    수업시작

---
+++ practice/08-data-structure/01-string.py
@@ -0,0 +1,62 @@
+# find
+text = 'banana'
+text.find
+
+# index
+
+# isupper
+
+# islower
+
+# isalpha
+
+# replace
+text = 'Hello, world!'
+
+# strip
+text = '  Hello, world!  '
+
+# split
+text = 'Hello, world!'
+
+# join
+words = ['Hello', 'world!']
+
+# capitalize
+text = 'heLLo, woRld!'
+
+# title
+# upper
+# lower
+# swapcase
+
+
+# 참고
+# isdecimal() : 가장 엄격한 기준을 적용, 오직 일반적인 십진수 숫자(0-9)만 True로 인식
+print("isdecimal() 메서드 예시:")
+print("'12345'.isdecimal():", '12345'.isdecimal())
+print("'123.45'.isdecimal():", '123.45'.isdecimal())
+print("'-123'.isdecimal():", '-123'.isdecimal())
+print("'Ⅳ'.isdecimal():", 'Ⅳ'.isdecimal())
+print("'½'.isdecimal():", '½'.isdecimal())
+print("'²'.isdecimal():", '²'.isdecimal())
+print()
+
+# isdigit() : 일반 숫자뿐만 아니라 지수 표현(²)도 True로 인식
+print("isdigit() 메서드 예시:")
+print("'12345'.isdigit():", '12345'.isdigit())
+print("'123.45'.isdigit():", '123.45'.isdigit())
+print("'-123'.isdigit():", '-123'.isdigit())
+print("'Ⅳ'.isdigit():", 'Ⅳ'.isdigit())
+print("'½'.isdigit():", '½'.isdigit())
+print("'²'.isdigit():", '²'.isdigit())
+print()
+
+# isnumeric() : 일반 숫자, 로마 숫자, 분수, 지수 등 다양한 형태의 숫자 표현을 True로 인식
+print("isnumeric() 메서드 예시:")
+print("'12345'.isnumeric():", '12345'.isnumeric())
+print("'123.45'.isnumeric():", '123.45'.isnumeric())
+print("'-123'.isnumeric():", '-123'.isnumeric())
+print("'Ⅳ'.isnumeric():", 'Ⅳ'.isnumeric())
+print("'½'.isnumeric():", '½'.isnumeric())
+print("'²'.isnumeric():", '²'.isnumeric())
---
+++ practice/08-data-structure/02-list.py
@@ -0,0 +1,32 @@
+# append
+my_list = [1, 2, 3]
+
+# extend
+my_list = [1, 2, 3]
+
+# insert
+my_list = [1, 2, 3]
+
+# remove
+my_list = [1, 2, 3]
+
+# pop
+my_list = [1, 2, 3, 4, 5]
+
+# clear
+my_list = [1, 2, 3]
+
+# index
+my_list = [1, 2, 3]
+
+# count
+my_list = [1, 2, 2, 3, 3, 3]
+
+# reverse
+my_list = [1, 3, 2, 8, 1, 9]
+
+# sort
+my_list = [3, 2, 100, 1]
+
+
+# sort(내림차순 정렬)
