be665538675e4dfbc254c30018a702190b95557b
Yu-jiin <jiin4083@naver.com>
2024-07-23 17:04:36 +0900

    swea 2071,2072 완료

--- py_study/codes/08-data-structure/99-dict-practice-01.py
+++ py_study/codes/08-data-structure/99-dict-practice-01.py
@@ -2,23 +2,32 @@
 blood_types = ['A', 'B', 'O', 'AB', 'A', 'O', 'B', 'A', 'AB', 'O', 'A', 'B']
 
 
-
 # 1. [] 표기법을 사용한 방법
+# 1. 변수이름 개떡같이 쓰지말기 !!!! 
 def count_blood_types(blood_types):
     blood = {}
+    
     for b in blood_types:
-        if b in blood:
-            blood[blood] += 1
-        else :
-            blood[blood] = 1 
+        if b not in blood:
+            blood[b] = 1
+        else:
+            blood[b] += 1
     return blood
+    
 
-print(count_blood_types(blood_types))  # {'A': 4, 'B': 3, 'O': 3, 'AB': 2}
+print(count_blood_types(blood_types)) 
+# {'A': 4, 'B': 3, 'O': 3, 'AB': 2}
 
 
+#  if 문을 get 으로 
 # 2. get() 메서드를 사용한 방법
-def count_blood_types(blood_types):
-    pass
+blood_types = ['A', 'B', 'O', 'AB', 'A', 'O', 'B', 'A', 'AB', 'O', 'A', 'B']
 
-
-print(count_blood_types(blood_types))  # {'A': 4, 'B': 3, 'O': 3, 'AB': 2}
+def count_blood_types3(blood_types):
+    new = {}
+    for k in blood_types:
+        new[k] = new.get(k,0)+1  # get이 if문의 역할 수행하는 느낌 
+        # new[k] = 키 할당     
+    return new
+        
+print(count_blood_types3(blood_types))  # {'A': 4, 'B': 3, 'O': 3, 'AB': 2}
