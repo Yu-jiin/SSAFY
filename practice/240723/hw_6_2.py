f0444237fea1e246485f1bc4a6ceffb0133c843d
Yu-jiin <jiin4083@naver.com>
2024-07-23 11:27:50 +0900

    1번째 과제 완료 Lv2

--- practice/240723/hw_6_2.py
+++ practice/240723/hw_6_2.py
@@ -1,5 +1,11 @@
 # 아래 함수를 수정하시오.
-def remove_duplicates_to_set():
+# 리스트를 set으로 변환하기 
+def remove_duplicates_to_set(my_list):
+    
+    my_set = set(my_list)
+    
+    print(my_set)
+    
     pass
 
 
