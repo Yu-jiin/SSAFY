4b17650824971eaad0f157e780cbfeebf2f085f0
Yu-jiin <jiin4083@naver.com>
2024-07-23 12:24:24 +0900

    9번째 실습 완료

--- practice/240723/ws_6_4.py
+++ practice/240723/ws_6_4.py
@@ -1,6 +1,10 @@
 # 아래 함수를 수정하시오.
-def get_keys_from_dict():
-    pass
+def get_keys_from_dict(my_dict):
+    result = (my_dict.keys())
+    a = []
+    for re in result:
+        a.append(re)
+    return a
 
 
 my_dict = {'name': 'Alice', 'age': 25}
