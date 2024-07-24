73f1bc9b2183017df39f60c139bdffaa09267962
Yu-jiin <jiin4083@naver.com>
2024-07-24 21:09:41 +0900

    7번째 과제 완료

---
+++ practice/240724/ws_7_4.py
@@ -0,0 +1,23 @@
+# 아래 클래스를 수정하시오.
+class Shape:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def print_info(self):
+        print(f'Width : {self.x}')
+        print(f'Height : {self.y}')
+        area = self.x * self.y
+        print(f'Area : {area}')
+        perimeter = (self.x + self.y)
+        print(f'Perimeter : {perimeter}')
+        
+
+    
+
+    
+
+
+shape1 = Shape(5, 3)
+shape1.print_info()
