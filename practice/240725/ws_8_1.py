3e5613b06308654ccc64c99de236211db248843c
Yu-jiin <jiin4083@naver.com>
2024-07-25 11:10:47 +0900

    1번째 실습 완료

---
+++ practice/240725/ws_8_1.py
@@ -0,0 +1,21 @@
+# 아래 클래스를 수정하시오.
+class Animal:
+    pass
+
+
+class Dog:
+    pass
+
+
+class Cat:
+    pass
+
+
+class Pet:
+    pass
+
+
+dog = Dog()
+print(Pet.access_num_of_animal())
+cat = Cat()
+print(Pet.access_num_of_animal())
